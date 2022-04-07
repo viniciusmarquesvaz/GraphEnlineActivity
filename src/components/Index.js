80612d9de4c04ed6fa75e15e800759685bfba2dd
Vinícius Marques Vaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2022-04-07 18:33:25 -0300

    Props

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,43 +1,22 @@
 import { plot } from "plotly.js/lib/bar";
 import React, { useState, useEffect } from "react";
+import GraphLayouts from "./GraphLayouts";
+
+const Index = (props) => {
+  
+  
+  const redSample = [{ redX: [0, 0, 0, 0, 0], redY: [1, 2, 3, 4, 5] }];
+  const greenSample = [{ greenX: [10, 100, 37, 10, 6], greenY: [1, 2, 3, 4, 5] }];
+  const markSample = [ { markerX:[39,90,87,40,2], markerY: [1,2,3,4,5] } ];
 
 
+  return (
+    //botoes
+    <div>
+        <GraphLayouts redSample={redSample} teste="teste"/>
 
-function Index() {
-    //use states para alterar com eventor a linha do gŕáfico
-    const [counterRed, setRed] = useState(false);
-    const showOrHideRed = () => setRed(true);
+    </div>
+  );
+};
 
-    const [counterGreen, setGreen] = useState(false);
-    const showOrHideGreen = () => setGreen(true)
-
-    const [counterMarker, setMarker] = useState(false);
-    const showOrHideMarker = () => setMarker(true)
-
-
-    return(
-        
-        //botoes 
-        <div>
-            <button >gerar dados aleatórios</button>
-
-            <button onClick={showOrHideRed} >botao linha vermelha</button>
-            {/* condição abaixo para mostrar ou não mostrar(rever) */}
-            { counterRed ?  <plot id='redLine' /> : null }
-
-
-            <button onClick={showOrHideGreen}>botao linha verde</button>
-            {/* condição abaixo para mostrar ou não mostrar(rever) */}
-            { counterGreen ?  <plot id='greenLine' /> : null }
-
-
-            <button onClick={showOrHideMarker} >botao marcador</button>
-            {/* condição abaixo para mostrar ou não mostrar(rever) */}
-            { counterMarker ?  <plot id='marker' /> : null }
-        </div>
-       
-     
-    )
-}
-
-export default Index
+export default Index;

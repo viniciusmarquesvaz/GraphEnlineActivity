6c1be51170644d5c401c592ca22ec8ab91deec24
Vinícius Marques Vaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2022-04-07 18:32:45 -0300

    Props

--- src/components/GraphLayouts.js
+++ src/components/GraphLayouts.js
@@ -1,49 +1,52 @@
-import React, { Component } from "react";
-import PLot from 'react-plotly.js';
+import React from "react";
+import Plot from 'react-plotly.js';
 import Index from "./Index";
 
 
 
-class GraphLayout extends Component {
-    render(){
+//const GraphLayout =  ({redX, redY , greenX, greenY, markerX ,markerY}) => {
+  const GraphLayout = (props) => {     
+      console.log(props) 
+   const {redSample}=props;
+   console.log(redSample);
         return (
             <div id="graph">
-            <PLot  data={[
-                    //linha vermelha
+            <Plot  data={[
+                    
                     {  id: 'redLine',
                         type: 'scartter',
                      mode: 'lines+markers',
                      marker: {color: 'red'},
-                        x: ['1','2','3','4','5'],
-                        y: [29,100,41,48,74]},
-                    //linha verde
+                        x:[1,2,3],
+                        y: [1,2,3]},
+                    
                         { id: 'greenLine',
                         type: 'scatter',
                          mode: 'lines+markers',
                          marker: {color: 'green'},
-                        x: ['1','2','3','4','5'],
-                        y: [40,20,100,68,10]},
-                    //marcador    
+                        x: [1,2,3],
+                        y: [1,2,3]},
+                     
                     {   id: 'markerLine',
                         type: 'scatter',
                          mode: 'markers',
                          marker: {color: 'orange'},
                          size: 12,
                          width: 3,
-                        x: ['1','2','3','4','5'],
-                        y: [15,84,46,13,97]} 
+                        x: [1,2,3],
+                        y: [1,2,3]} 
                         
                 ]} 
                 layout={ { width: 1000,height: 500} } />
-                {/* todos os botões */}
-                <Index/>
+                
+               
 
 
             </div>
             
         )
         
-    }
+    
 }
 
 export default GraphLayout;
